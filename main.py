5b0465e9a6a8c9c516bb534a1d8062f6f0bed293
spekie <gbarkar@proton.me>
2025-02-14 08:14:17 -0500

    init

---
+++ main.py
@@ -0,0 +1,26 @@
+import requests
+import inspect
+
+from discord import app_commands, Intents, Client, Interaction
+
+class client(Client):
+    def __init__(self, *, intents: Intents):
+        super().__init__(intents=intents)
+        self.tree = app_commands.CommandTree(self)
+
+    async def setup_hook(self) -> None:
+        await self.tree.sync()
+
+client = client(intents=Intents.none())
+
+@client.event
+async def on_ready():
+    print(inspect.cleandoc(f"Bot name: {client.user}\nBot ID: {client.user.id}\n\nInvite link below\nhttps://discord.com/api/oauth2/authorize?client_id={client.user.id}&scope=applications.commands%20bot"))
+
+@client.tree.command()
+async def hello(interaction: Interaction):
+    " Wow you read this? "
+    print(f"> {interaction.user} used the /hello command.")
+    await interaction.response.send_message(inspect.cleandoc(f"Hi **{interaction.user}**."))
+
+client.run('INSERT YOUR DISCORD BOT TOKEN HERE')
